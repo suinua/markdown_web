be77dfa8f726526b5b2f9bb00b4f5e2d8586d54a
suinua <maatinosippo@gmail.com>
2022-02-21 15:03:51 +0900

    test

--- setup.sh
+++ setup.sh
@@ -5,10 +5,7 @@ mkdir "output"
 cp -r /assets/* output
 dart2js -O2 -o output/main.js web/main.dart
 dart bin/markdown_web.dart
-path="suinua/sample_code"
-path=`echo ${GITHUB_REPOSITORY//*\//}`
+path=${GITHUB_REPOSITORY//*\//}
 echo $path
-cp -r output $path/output
-cd home/runner/work/$path/$path
 pwd
 ls
