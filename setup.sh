fd89b5cafff9eeaafd553a8ffce87e7742ce320d
suinua <maatinosippo@gmail.com>
2022-02-21 15:59:50 +0900

    test

--- setup.sh
+++ setup.sh
@@ -1,7 +1,15 @@
-#!/bin/bash
+#!/bin/sh -l
 cd ../../../../
 dart pub get
 mkdir "output"
 cp -r /assets/* output
 dart2js -O2 -o output/main.js web/main.dart
 dart bin/markdown_web.dart
+echo $GITHUB_ACTION_PATH
+echo $GITHUB_ACTION_REPOSITORY
+echo $GITHUB_ENV
+echo $GITHUB_PATH
+#cp -r output home/runner/work/$REPO_NAME/$REPO_NAME/output
+#cd home/runner/work/$REPO_NAME/$REPO_NAME
+#pwd
+#ls
