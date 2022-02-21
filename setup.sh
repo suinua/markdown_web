1ca6d1735f767008dec37cfaa4297a64ccddd66e
suinua <maatinosippo@gmail.com>
2022-02-21 15:27:07 +0900

    test

--- setup.sh
+++ setup.sh
@@ -5,8 +5,4 @@ mkdir "output"
 cp -r /assets/* output
 dart2js -O2 -o output/main.js web/main.dart
 dart bin/markdown_web.dart
-echo home/runner/work/$REPO_NAME/$REPO_NAME
-cp -r output home/runner/work/$REPO_NAME/$REPO_NAME/output
-cd home/runner/work/$REPO_NAME/$REPO_NAME
-pwd
-ls
+echo ${{ github.event.repository.name }}
