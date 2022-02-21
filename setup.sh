9434b0c650fd8c5460203d023575094d6e095905
suinua <maatinosippo@gmail.com>
2022-02-21 16:19:14 +0900

    test

--- setup.sh
+++ setup.sh
@@ -6,5 +6,4 @@ cp -r /assets/* output
 dart2js -O2 -o output/main.js web/main.dart
 dart bin/markdown_web.dart
 #cp -r output home/runner/work/$INPUT_REPOSITORYNAME/$INPUT_REPOSITORYNAME/output
-ls
 ls home
