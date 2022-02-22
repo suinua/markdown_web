618ab1daba4c57531ecae5c4d509068b1fcab9cf
suinua <maatinosippo@gmail.com>
2022-02-22 11:27:49 +0900

    update

--- web/model/article.dart
+++ web/model/article.dart
@@ -21,4 +21,13 @@ class Tag {
   final String text;
 
   Tag(this.text);
+
+  @override
+  bool operator ==(Object other) {
+    if (other is Tag) {
+      return text == other.text;
+    }
+
+    return false;
+  }
 }
