e063dfd25b9e615a8abfcfc97f952f7d0e5880e5
suinua <maatinosippo@gmail.com>
2022-02-22 11:28:03 +0900

    シングルトンからsttaicに

--- web/pool/articles_pool.dart
+++ web/pool/articles_pool.dart
@@ -5,20 +5,15 @@ import '../model/folder.dart';
 
 class ArticlesPool {
   static Folder? _data;
-  static ArticlesPool? _instance;
-
-  ArticlesPool._internal() {
-    _init();
-  }
-
-  factory ArticlesPool() {
-    _instance ??= ArticlesPool._internal();
-    return _instance!;
-  }
 
   static void _init() {
     HttpRequest.getString('data.json').then((value) {
       _data = Folder.fromMap(jsonDecode(value));
     });
+  }
+
+  static Folder data() {
+    if (_data == null) _init();
+    return _data!;
   }
 }
