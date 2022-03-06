680fe6a50f921fe2bb70f7296f704aa2747a8d03
suinua <34801662+suinua@users.noreply.github.com>
2022-03-06 17:54:58 +0900

    update

--- web/main.dart
+++ web/main.dart
@@ -1,5 +1,14 @@
+import 'dart:html';
+
 import '../common/view/folder_structure_menu_view.dart';
 
 void main() {
   FolderStructureMenuViewController.setUp();
+
+  querySelector('.github-button')!.onClick.listen((event) {
+    var userName = window.location.host.replaceAll(RegExp('(.*)\.'), '');
+    var repositoryName = window.location.pathname?.replaceFirst('/', '').replaceFirst(RegExp('/(.*)'), '');
+    var url = 'https://github.com/$userName/$repositoryName';
+    window.open(url, '_blank');
+  });
 }
