a9b671a997b9327fac4f2e9a7afd1ece5a496790
suinua <34801662+suinua@users.noreply.github.com>
2022-03-06 17:58:57 +0900

    fix

--- web/main.dart
+++ web/main.dart
@@ -6,7 +6,7 @@ void main() {
   FolderStructureMenuViewController.setUp();
 
   querySelector('.github-button')!.onClick.listen((event) {
-    var userName = window.location.host.replaceAll(RegExp('(.*)\.'), '');
+    var userName = window.location.host.replaceAll(RegExp('\.(.*)'), '');
     var repositoryName = window.location.pathname?.replaceFirst('/', '').replaceFirst(RegExp('/(.*)'), '');
     var url = 'https://github.com/$userName/$repositoryName';
     window.open(url, '_blank');
