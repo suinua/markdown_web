e61362cb0d0b05be58cd19fbf966ad7fc3957f8b
suinua <34801662+suinua@users.noreply.github.com>
2022-03-11 21:15:42 +0900

    スマホに対応

--- common/view/article_page.dart
+++ common/view/article_page.dart
@@ -17,7 +17,7 @@ class ArticlePage {
 <html lang="ja">
 
 <head>
-    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1" charset="UTF-8">
     <title>${article.title}</title>
     <link rel="stylesheet" href="${path}main.css">
     <link rel="stylesheet" href="${path}article_menu.css">
